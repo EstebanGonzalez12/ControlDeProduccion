d67ced57afc16b5d2623b8b2b2b6f87d48885ca6
estebanG <esgonzalezor@cesde.net>
2024-10-27 20:53:22 -0500

    Se avanza con la funcion capturarProduccionDiaria, hasta la parte donde se le pide al usuario ingresar los valores

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
-const usuarioValido = "admin";
-const contrasenaValida = "1234";
-let intentos = 0;
-const maxIntentos = 3;
-
 document.getElementById("btn").addEventListener("click", iniciarSesion),
   function iniciarSesion() {
+    const usuarioValido = "admin";
+    const contrasenaValida = "1234";
+    let intentos = 0;
+    const maxIntentos = 3;
+
     const usuario = document.getElementById(usuario).value;
     const contrasena = document.getElementById(contraseña).value;
 
     if (usuario === usuarioValido && contrasena === contrasenaValida) {
-      alert("Se ha iniciado sesion")
-    } else intentos++
+      alert("Se ha iniciado sesion");
+    } else intentos++;
 
     if (intentos >= maxIntentos) {
-      alert("Superaste los 3 intentos. Cerrando la aplicacion")
+      alert("Superaste los 3 intentos. Cerrando la aplicacion");
     }
-    window.close()
-  }
+    window.close();
+  };
 
+function capturarProduccionDiaria() {
+    let dia = 1;
+    const resultado = document.getElementById("resultado")
+
+    while(dia<=7){
+        let unidades = parseInt(prompt("Ingresa la cantidad de unidades producidas: "))
+    }
+}
